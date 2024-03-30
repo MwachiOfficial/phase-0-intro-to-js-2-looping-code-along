63e78bbdcf688e11cb16b4a796ae65cbd4d7f121
matthewmwachi <mattmuhande@gmail.com>
2024-03-30 22:12:00 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(data, event){
+    let array = []
+    for(let i = 0; i < data.length; i++){
+        array.push(`Thank you, ${data[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+      number-=1
+    }
+    return countDown
+}
